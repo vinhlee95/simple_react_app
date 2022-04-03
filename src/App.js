657b3cd0b261439fdc1a9b9b46c651f46620fd47
Vinh Le <lethanhvinh95@gmail.com>
2022-04-03 10:50:57 +0300

    make test passed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         >
           Learn React
         </a>
-        <p>New change</p>
       </header>
     </div>
   );
